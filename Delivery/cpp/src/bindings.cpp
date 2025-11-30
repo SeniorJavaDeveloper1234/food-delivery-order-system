61e045c3e116a003a68b058cc38947dbaaeb18d9
Volodymyr <sss@>
2025-11-30 16:30:41 +0200

    more bindings

--- Delivery/cpp/src/bindings.cpp
+++ Delivery/cpp/src/bindings.cpp
@@ -1,20 +1,48 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "DeliverySystem.h"
 #include "Client.h"
 #include "Courier.h"
 #include "MenuItem.h"
+#include "OrderItem.h"
 #include "Order.h"
+#include "ClientRepository.h"
+#include "CourierRepository.h"
+#include "MenuRepository.h"
+#include "OrderRepository.h"
+#include "DeliverySystem.h"
 
 namespace py = pybind11;
+
+
+std::string orderStatusToString(OrderStatus s) {
+    switch (s) {
+    case OrderStatus::Pending:    return "Pending";
+    case OrderStatus::Preparing:  return "Preparing";
+    case OrderStatus::Delivering: return "Delivering";
+    case OrderStatus::Completed:  return "Completed";
+    case OrderStatus::Cancelled:  return "Cancelled";
+    }
+    return "Unknown";
+}
+
 
 PYBIND11_MODULE(delivery, m)
 {
     m.doc() = "Delivery System Python bindings";
 
+
+    py::enum_<OrderStatus>(m, "OrderStatus")
+        .value("Pending", OrderStatus::Pending)
+        .value("Preparing", OrderStatus::Preparing)
+        .value("Delivering", OrderStatus::Delivering)
+        .value("Completed", OrderStatus::Completed)
+        .value("Cancelled", OrderStatus::Cancelled);
+
+
     py::class_<Client>(m, "Client")
-        .def(py::init<int, const std::string&, const std::string&, const std::string&, const std::string&>())
+        .def(py::init<int, const std::string&, const std::string&,
+            const std::string&, const std::string&>())
         .def("getId", &Client::getId)
         .def("getFirstName", &Client::getFirstName)
         .def("getLastName", &Client::getLastName)
@@ -22,12 +50,14 @@ PYBIND11_MODULE(delivery, m)
         .def("getAddress", &Client::getAddress);
 
     py::class_<Courier>(m, "Courier")
-        .def(py::init<int, const std::string&, const std::string&, const std::string&, bool>())
+        .def(py::init<int, const std::string&, const std::string&,
+            const std::string&, bool>())
         .def("getId", &Courier::getId)
         .def("getFirstName", &Courier::getFirstName)
         .def("getLastName", &Courier::getLastName)
         .def("getPhone", &Courier::getPhone)
         .def("isAvailable", &Courier::isAvailable);
+
 
     py::class_<MenuItem>(m, "MenuItem")
         .def(py::init<int, const std::string&, const std::string&, double>())
@@ -36,12 +66,38 @@ PYBIND11_MODULE(delivery, m)
         .def("getDescription", &MenuItem::getDescription)
         .def("getPrice", &MenuItem::getPrice);
 
+    py::class_<OrderItem>(m, "OrderItem")
+        .def(py::init<int, const std::string&, double, int>())
+        .def("getMenuItemId", &OrderItem::getMenuItemId)
+        .def("getName", &OrderItem::getName)
+        .def("getUnitPrice", &OrderItem::getUnitPrice)
+        .def("getQuantity", &OrderItem::getQuantity)
+        .def("getTotalPrice", &OrderItem::getTotalPrice);
+
     py::class_<Order>(m, "Order")
         .def("getId", &Order::getId)
         .def("getClientId", &Order::getClientId)
         .def("getCourierId", &Order::getCourierId)
         .def("getStatus", &Order::getStatus)
-        .def("getTotalPrice", &Order::getTotalPrice);
+        .def("getStatusString", [](const Order& o) { return orderStatusToString(o.getStatus()); })
+        .def("getItems", &Order::getItems)
+        .def("getTotalPrice", &Order::getTotalPrice)
+        .def("getCreatedAt", &Order::getCreatedAt);
+
+    py::class_<ClientRepository>(m, "ClientRepository")
+        .def("getAll", &ClientRepository::getAll);
+
+    py::class_<CourierRepository>(m, "CourierRepository")
+        .def("getAll", &CourierRepository::getAll);
+
+    py::class_<MenuRepository>(m, "MenuRepository")
+        .def("getAll", &MenuRepository::getAll);
+
+    py::class_<OrderRepository>(m, "OrderRepository")
+        .def("getAll", &OrderRepository::getAll)
+        .def("findByClientId", &OrderRepository::findByClientId)
+        .def("findByCourierId", &OrderRepository::findByCourierId);
+
 
     py::class_<DeliverySystem>(m, "DeliverySystem")
         .def(py::init<>())
@@ -51,7 +107,12 @@ PYBIND11_MODULE(delivery, m)
         .def("cancelOrder", &DeliverySystem::cancelOrder)
         .def("getClientOrders", &DeliverySystem::getClientOrders)
         .def("getCourierOrders", &DeliverySystem::getCourierOrders)
-        .def("clients", &DeliverySystem::clients, py::return_value_policy::reference)
-        .def("couriers", &DeliverySystem::couriers, py::return_value_policy::reference)
-        .def("menu", &DeliverySystem::menu, py::return_value_policy::reference);
+        .def("clients", &DeliverySystem::clients,
+            py::return_value_policy::reference)
+        .def("couriers", &DeliverySystem::couriers,
+            py::return_value_policy::reference)
+        .def("menu", &DeliverySystem::menu,
+            py::return_value_policy::reference)
+        .def("orders", &DeliverySystem::orders,
+            py::return_value_policy::reference);
 }
